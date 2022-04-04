f89b16831842173ba6bcbed570ad12c63fbe3a41
ribeirosofia <sofia.ribeiro@creditas.com>
2022-04-04 15:51:43 -0300

    Adiçao de console.log para a constante idade e adiçao da constante idadeSomada

---
+++ primeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade + 2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada)
