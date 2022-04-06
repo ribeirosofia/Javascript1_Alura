28582d6f9732c9d7b4ac44c924767c5adfd3dfe7
ribeirosofia <sofia.ribeiro@creditas.com>
2022-04-06 12:36:03 -0300

    Atribuiçao de variaveis const e let

---
+++ atribuindoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 27;
+const primeiroNome = "Sofia";
+const sobrenome = "Ribeiro";
+
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+console.log(primeiroNome, sobrenome);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let contador = 0;
